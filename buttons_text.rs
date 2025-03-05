0c337a0e895f2b602ab863bc7e6f5d640be5ada0
Mathew <44036272+Mathew-D@users.noreply.github.com>
2025-03-05 15:49:59 -0500

    Update buttons_text.rs
    added option to change enabled and it picks a color between main and GRAY

--- buttons_text.rs
+++ buttons_text.rs
@@ -32,13 +32,17 @@ pub struct TextButton {
     pub width: f32,
     pub height: f32,
     pub text: String,
+    pub enabled: bool,
     pub normal_color: Color,
     pub hover_color: Color,
+    off_color: Color,
 }
 
 impl TextButton {
     pub fn new(x: f32, y: f32, width: f32, height: f32, text: String, normal_color: Color, hover_color: Color) -> Self {
-        Self { x, y, width, height, text, normal_color, hover_color }
+        let enabled = true;
+        let off_color = lerp_color(normal_color, GRAY, 0.5);
+        Self { x, y, width, height, text, enabled, normal_color, hover_color,off_color }
     }
 
     pub fn click(&self) -> bool {
@@ -51,10 +55,14 @@ impl TextButton {
         let is_hovered = rect.contains(mouse_pos);
 
         // Draw the text button (change color on hover)
-        let button_color = if is_hovered {
-            self.hover_color
+        let button_color = if self.enabled {
+            if is_hovered {
+                self.hover_color
+            } else {
+                self.normal_color
+            }
         } else {
-            self.normal_color
+            self.off_color
         };
 
         draw_rectangle(self.x, self.y, self.width, self.height, button_color);
@@ -72,4 +80,12 @@ impl TextButton {
         // After drawing, check if the button was clicked
         is_hovered && is_mouse_button_pressed(MouseButton::Left)
     }
+}
+fn lerp_color(c1: Color, c2: Color, factor: f32) -> Color {
+    Color::new(
+        c1.r * (1.0 - factor) + c2.r * factor,
+        c1.g * (1.0 - factor) + c2.g * factor,
+        c1.b * (1.0 - factor) + c2.b * factor,
+        1.0,
+    )
 }
