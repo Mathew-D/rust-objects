7c648b5a3e2ca05a596f889ec929282413b36161
Mathew <44036272+Mathew-D@users.noreply.github.com>
2025-04-26 12:57:50 -0400

    Update inputs.rs
    Added delete key and font size and colors.

--- inputs.rs
+++ inputs.rs
@@ -1,15 +1,24 @@
 /*
 Made by: Mathew Dusome
-Feb 6 2025
+April 26 2025
 To import you need:
 Adds a text input object
-mod objects {
-    pub mod text_input;
-}
-use objects::text_input::TextBox;
+At the top of your main.rs file you need to add:
+    mod objects {
+        pub mod text_input;
+    }
+
+Add with the other use statements
+    use objects::text_input::TextBox;
 
 Then to use you would go:
-let mut textbox = TextBox::new(100.0, 100.0, 300.0, 40.0);
+    let mut textbox = TextBox::new(100.0, 100.0, 300.0, 40.0,50.0);
+Where the numbers are x, y, width, height, font size
+You can also set the colors of the text box by using:
+    .with_colors(WHITE, BLUE, DARKGRAY, RED);
+Where the colors are text color, border color, background color, and cursor color respectively.
+
+Then in the loop you would do:
 
     textbox.update();
     textbox.draw();
@@ -23,14 +32,18 @@ pub struct TextBox {
     pub height: f32,
     pub text: String,
     pub active: bool,
-    cursor_index: usize,
-    cursor_timer: f32,
-    cursor_visible: bool,
+    pub cursor_index: usize,
+    pub cursor_timer: f32,
+    pub cursor_visible: bool,
+    pub font_size: f32, // Font size field
+    pub text_color: Color, // Text color field
+    pub border_color: Color, // Border color field
+    pub background_color: Color, // Background color field
+    pub cursor_color: Color, // Cursor color field
 }
 
-
 impl TextBox {
-    pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
+    pub fn new(x: f32, y: f32, width: f32, height: f32, font_size: f32) -> Self {
         Self {
             x,
             y,
@@ -41,119 +54,126 @@ impl TextBox {
             cursor_index: 0,
             cursor_timer: 0.0,
             cursor_visible: true,
+            font_size,
+            text_color: BLACK, // Default color for text
+            border_color: DARKGRAY, // Default color for border
+            background_color: LIGHTGRAY, // Default color for background
+            cursor_color: BLACK, // Default color for cursor
         }
     }
     
+    // Add a method to change colors
+    #[allow(unused)]
+    pub fn with_colors(mut self, text_color: Color, border_color: Color, background_color: Color, cursor_color: Color) -> Self {
+        self.text_color = text_color;
+        self.border_color = border_color;
+        self.background_color = background_color;
+        self.cursor_color = cursor_color;
+        self
+    }
+
     pub fn update(&mut self) {
         if is_mouse_button_pressed(MouseButton::Left) {
             let (mx, my) = mouse_position();
             self.active = mx >= self.x && mx <= self.x + self.width && my >= self.y && my <= self.y + self.height;
     
             if self.active {
-                let font_size = 20.0;
-                let mut current_x = self.x + 5.0;
+                // Clicking to place the cursor
+                let text_x = self.x + 5.0;
+                let mouse_pos = mx - text_x;
                 self.cursor_index = 0;
     
-                for (i, c) in self.text.char_indices() {
-                    let w = measure_text(&c.to_string(), None, font_size as u16, 1.0).width;
-                    if mx < current_x + w / 2.0 {
+                let mut cursor_offset = 0.0;
+                while self.cursor_index < self.text.len() {
+                    let char_width = measure_text(&self.text[self.cursor_index..self.cursor_index + 1], None, self.font_size as u16, 1.0).width;
+                    cursor_offset += char_width;
+                    if cursor_offset > mouse_pos {
                         break;
                     }
-                    current_x += w;
-                    self.cursor_index = i + c.len_utf8();
-                }
-    
-                // If mouse is past end, snap to end
-                if mx > current_x {
-                    self.cursor_index = self.text.len();
+                    self.cursor_index += self.text[self.cursor_index..].chars().next().unwrap().len_utf8();
                 }
             }
         }
     
         if self.active {
-            while let Some(character) = get_char_pressed() {
-                if character == '\u{8}' {
-                    // Backspace
-                    if self.cursor_index > 0 {
-                        let prev_index = self.text[..self.cursor_index]
-                            .char_indices()
-                            .rev()
-                            .next()
-                            .map(|(i, _)| i)
-                            .unwrap_or(0);
-                        self.text.replace_range(prev_index..self.cursor_index, "");
-                        self.cursor_index = prev_index;
-                    }
-                } else {
-                    self.text.insert(self.cursor_index, character);
-                    self.cursor_index += character.len_utf8();
+            // Handle typing
+            while let Some(c) = get_char_pressed() {
+                if !c.is_control() {
+                    self.text.insert(self.cursor_index, c);
+                    self.cursor_index += c.len_utf8();
                 }
             }
     
+            // Handle Delete
+            if is_key_pressed(KeyCode::Delete) && self.cursor_index < self.text.len() {
+                if let Some((_, c)) = self.text[self.cursor_index..].char_indices().next() {
+                    let char_len = c.len_utf8();
+                    self.text.replace_range(self.cursor_index..self.cursor_index + char_len, "");
+                }
+            }
+    
+            // Handle Backspace
+            if is_key_pressed(KeyCode::Backspace) && self.cursor_index > 0 {
+                if let Some((prev_offset, _c)) = self.text[..self.cursor_index].char_indices().rev().next() {
+                    self.text.replace_range(prev_offset..self.cursor_index, "");
+                    self.cursor_index = prev_offset;
+                }
+            }
+    
+            // Handle Arrow Keys (Left and Right)
             if is_key_pressed(KeyCode::Left) && self.cursor_index > 0 {
-                self.cursor_index = self.text[..self.cursor_index]
-                    .char_indices()
-                    .rev()
-                    .next()
-                    .map(|(i, _)| i)
-                    .unwrap_or(0);
+                let prev_char = self.text[..self.cursor_index].chars().last().unwrap();
+                let char_len = prev_char.len_utf8();
+                self.cursor_index -= char_len;
             }
     
             if is_key_pressed(KeyCode::Right) && self.cursor_index < self.text.len() {
-                self.cursor_index += self.text[self.cursor_index..]
-                    .char_indices()
-                    .next()
-                    .map(|(_i, c)| c.len_utf8())
-                    .unwrap_or(0);
+                let next_char = self.text[self.cursor_index..].chars().next().unwrap();
+                let char_len = next_char.len_utf8();
+                self.cursor_index += char_len;
             }
-    
-          
-    
-            if is_key_pressed(KeyCode::Home) {
-                self.cursor_index = 0;
-            }
-    
-            if is_key_pressed(KeyCode::End) {
-                self.cursor_index = self.text.len();
-            }
-    
             self.cursor_timer += get_frame_time();
             if self.cursor_timer >= 0.5 {
                 self.cursor_visible = !self.cursor_visible;
                 self.cursor_timer = 0.0;
-            }
+            } 
         } else {
-            self.cursor_visible = false;
+            self.cursor_visible = false; 
         }
-    }
         
+    }
     
-
     pub fn draw(&self) {
-        draw_rectangle(self.x, self.y, self.width, self.height, LIGHTGRAY);
+        let padding = 5.0;
+        let text_x = self.x + padding;
+        let text_y = self.y + self.height / 2.0 + self.font_size / 2.5;
     
-        let font_size = 20.0;
-        let text_x = self.x + 5.0;
-        let text_y = self.y + self.height / 2.0 + 5.0;
-    
-        draw_text(&self.text, text_x, text_y, font_size, BLACK);
+        // Draw the background and border with customizable colors
+        draw_rectangle(self.x, self.y, self.width, self.height, self.background_color);
+        draw_text(&self.text, text_x, text_y, self.font_size, self.text_color);
     
         if self.active && self.cursor_visible {
-            let cursor_text = &self.text[..self.cursor_index];
-            let cursor_offset = measure_text(cursor_text, None, font_size as u16, 1.0).width;
+            let mut cursor_offset = 0.0;
+            if self.cursor_index > 0 {
+                let cursor_text = &self.text[..self.cursor_index];
+                for c in cursor_text.chars() {
+                    cursor_offset += measure_text(&c.to_string(), None, self.font_size as u16, 1.0).width;
+                }
+            }
+    
+            // Draw the cursor with customizable color
             draw_line(
                 text_x + cursor_offset,
-                text_y - font_size,
+                text_y - self.font_size,
                 text_x + cursor_offset,
                 text_y + 5.0,
                 2.0,
-                BLACK,
+                self.cursor_color,
             );
         }
     
-        let border_color = if self.active { BLUE } else { DARKGRAY };
-        draw_rectangle_lines(self.x, self.y, self.width, self.height, 2.0, border_color);
+        // Draw the border with customizable color
+        draw_rectangle_lines(self.x, self.y, self.width, self.height, 2.0, self.border_color);
     }
-    
-    
 }
+
