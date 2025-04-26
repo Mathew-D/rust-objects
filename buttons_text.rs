180da4b65db4022b42e03e7d4ef328880e1721a1
Mathew <44036272+Mathew-D@users.noreply.github.com>
2025-04-26 14:03:38 -0400

    Update buttons_text.rs
    fix it so you just pass a str not a full string so it is easier

--- buttons_text.rs
+++ buttons_text.rs
@@ -2,22 +2,24 @@
 Made by: Mathew Dusome
 Feb 6 2025
 To import you need:
-Adds a button object 
+Adds a button object
 mod objects {
     pub mod txt_buttons;
 }
 use objects::txt_buttons::TextButton;
 
-Then to use you would go: 
+Then to use you would go:
+    // With default colors:
     let text_button = TextButton::new(
         100.0,
         200.0,
         200.0,
         60.0,
-        "Click Me".to_string(),
+        "Click Me",
         BLUE,
         GREEN,
     );
+
 Then:
 if text_button.click() {
 
@@ -39,10 +41,20 @@ pub struct TextButton {
 }
 
 impl TextButton {
-    pub fn new(x: f32, y: f32, width: f32, height: f32, text: String, normal_color: Color, hover_color: Color) -> Self {
+    pub fn new(x: f32, y: f32, width: f32, height: f32, text: impl Into<String>, normal_color: Color, hover_color: Color) -> Self {
         let enabled = true;
         let off_color = lerp_color(normal_color, GRAY, 0.5);
-        Self { x, y, width, height, text, enabled, normal_color, hover_color,off_color }
+        Self {
+            x,
+            y,
+            width,
+            height,
+            text: text.into(),
+            enabled,
+            normal_color,
+            hover_color,
+            off_color,
+        }
     }
 
     pub fn click(&self) -> bool {
@@ -69,23 +81,13 @@ impl TextButton {
 
         // Draw the text
         let text_width = measure_text(&self.text, None, 30, 1.0).width;
-        draw_text(
-            &self.text,
-            self.x + (self.width / 2.0) - (text_width / 2.0),
-            self.y + (self.height / 2.0),
-            30.0,
-            WHITE,
-        );
+        draw_text(&self.text, self.x + (self.width / 2.0) - (text_width / 2.0), self.y + (self.height / 2.0), 30.0, WHITE);
 
         // After drawing, check if the button was clicked
         is_hovered && self.enabled && is_mouse_button_pressed(MouseButton::Left)
     }
 }
 fn lerp_color(c1: Color, c2: Color, factor: f32) -> Color {
-    Color::new(
-        c1.r * (1.0 - factor) + c2.r * factor,
-        c1.g * (1.0 - factor) + c2.g * factor,
-        c1.b * (1.0 - factor) + c2.b * factor,
-        1.0,
-    )
+    Color::new(c1.r * (1.0 - factor) + c2.r * factor, c1.g * (1.0 - factor) + c2.g * factor, c1.b * (1.0 - factor) + c2.b * factor, 1.0)
 }
+
