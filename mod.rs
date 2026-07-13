cc81c53d0bf60db2a0c603e66afce32cfd53cbec
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-13 13:04:22 -0400

    grid and mod update comments

--- mod.rs
+++ mod.rs
@@ -1,15 +1,13 @@
 /*
 --------------------------------------------
-modules/mod.rs
-This file lists which modules (Rust files) are part of the "modules" folder.
+modules
+This file lists which modules (Rust files) are part of the folder it is named after.
 
 This file just tells Rust what's available. It's like a directory of all the tools.
 
 Example:
 pub mod grid;
 
-Once listed here, you can import from main.rs:
-use crate::modules::grid::draw_grid;
 --------------------------------------------
 */
 // Add modules below
